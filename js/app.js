9e1520d6d26a22c0fe30ceb686099c1036b4d90e
sparsh <sparshgr8@yahoo.com>
2021-12-14 10:39:20 +0530

    Added comments before fucntiions.

--- js/app.js
+++ js/app.js
@@ -16,7 +16,11 @@
 // get the secions
 // add the secions inside nav bar
 const SECTION_PREFIX = "SECTION";
+const ACTIVE_CLASS = "active"
 
+/** 
+ * Funtion to get the Navigation Items from Section Tag 
+ * */
 const getNavigationItems = () => {
   const navItems = [];
   const sections = document.getElementsByTagName("section");
@@ -27,17 +31,22 @@ const getNavigationItems = () => {
 };
 
 
-
+/**
+ * Scroll event used to navigate to section
+ * @param {*} sectionIndex : the index of section where we need to navigate to.
+ */
 const navigateToSection = (sectionIndex) => {
-  
   const section = document.getElementsByTagName("section")[sectionIndex];
   section.scrollIntoView({
     behavior: "smooth",
-    block: "center",
     inline: "center",
   });
 };
 
+
+/**
+ * Logic to add Navigation bar inside the nav bar list class
+ */
 const addNavigationBar = () => {
   const navBarParent = document.getElementById("navbar__list");
   const navItems = getNavigationItems();
@@ -48,17 +57,25 @@ const addNavigationBar = () => {
       navigateToSection(index);
     };
     if (index == 0) {
-      li.className = "active";
+      li.className = ACTIVE_CLASS;
     }
     navBarParent.appendChild(li);
   });
 };
-// check if element is visible
-const isInViewport =(el) => {
-    const rect = el.getBoundingClientRect();
+
+/**
+ * Logic to check if any section is in viewport
+ * @param {*} section: The scrollable section
+ * @returns boolean: true or false
+ */
+const isInViewport =(section) => {
+    const rect = section.getBoundingClientRect();
     return rect.top < window.innerHeight/2
 }
 
+/**
+ * Removing the last active class from section and list
+ */
 const removeLastActiveClasses = () => {
     const lastActiveSecton = document.querySelector("section.active");
     const lastActiveList =  document.querySelector("li.active");
@@ -66,6 +83,9 @@ const removeLastActiveClasses = () => {
     lastActiveList.className = "";
 }
 
+/**
+ * Manages the logic to add/ remove active classes
+ */
 const scrollListener = () => {
     const sections = document.getElementsByTagName("section");
     const lists = document.getElementsByTagName("li");
@@ -73,56 +93,18 @@ const scrollListener = () => {
         for (i = 0; i < sections.length; i++) {
             if(isInViewport(sections[i])) {
                 removeLastActiveClasses(i);
-                sections[i].className = "active";
-                lists[i].className = "active";
+                sections[i].className = ACTIVE_CLASS;
+                lists[i].className = ACTIVE_CLASS;
             }
           }
     }
     document.addEventListener('scroll', scroll);
 }
 
-
+/**
+ * Main Funtion
+ */
 window.onload = () => {
   addNavigationBar();
   scrollListener();
 };
-
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
- */
-
-/**
- * Define Global Variables
- *
- */
-
-/**
- * End Global Variables
- * Start Helper Functions
- *
- */
-
-/**
- * End Helper Functions
- * Begin Main Functions
- *
- */
-
-// build the nav
-
-// Add class 'active' to section when near top of viewport
-
-// Scroll to anchor ID using scrollTO event
-
-/**
- * End Main Functions
- * Begin Events
- *
- */
-
-// Build menu
-
-// Scroll to section on link click
-
-// Set sections as active
