b76cbd52a80a9c80ab9a6f893c5cdd13f249d52f
sparsh <sparshgr8@yahoo.com>
2021-12-14 10:30:23 +0530

    Simplified the isInsideViewport Logic and media query for section item

--- js/app.js
+++ js/app.js
@@ -56,12 +56,7 @@ const addNavigationBar = () => {
 // check if element is visible
 const isInViewport =(el) => {
     const rect = el.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
+    return rect.top < window.innerHeight/2
 }
 
 const removeLastActiveClasses = () => {
